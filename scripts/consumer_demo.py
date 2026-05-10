f4f2f2d889ce10ec542c50f6cd814e19b3e136f7
VVN8819 <vitalii8819@gmail.com>
2026-05-11 02:45:23 +0800

    features: Разделил структуру metrics от consumer_demo, расчет Воронки конверсии calculate_conversion_rates

--- scripts/consumer_demo.py
+++ scripts/consumer_demo.py
@@ -1,8 +1,6 @@
 import json
 from kafka import KafkaConsumer
-from collections import defaultdict, Counter
-import pandas as pd
-from datetime import datetime
+from metrics import BusinessMetrics
 import numpy as np
 
 consumer = KafkaConsumer(
@@ -13,23 +11,7 @@ consumer = KafkaConsumer(
     group_id='demo-analytics-group' # вспомнит, на каком письме он остановился, и не заставит читать всё заново
 )
 
-action_stats = Counter()
-product_views = Counter()
-user_sessions = defaultdict(list)
-
-# Добавлены доп метрики для отслеживания
-user_behavior = defaultdict(list) # История действий по пользователям
-session_data = defaultdict(dict) # Данные сессий
-hourly_activity = defaultdict(int) # Активность по часам
-conversion_funnel = Counter() # Воронка конверсии
-product_performance = defaultdict(lambda: {
-    'views': 0,
-    'cart_adds': 0,
-    'purchases': 0,
-    'revenue': 0
-})
-
-revenue_data = []
+metrics = BusinessMetrics()
 
 print("Starting to consume messages...")
 print("Press Ctrl+C to stop and see analytics")
@@ -39,123 +21,19 @@ message_count = 0
 try:
     for message in consumer:
         event = message.value
-        action = event['action']
         message_count += 1
-
-        # Обновляем статистику
-        action_stats[event['action']] += 1
-
-        if event.get('product'):
-            product_views[event['product']] += 1
-
-        user_sessions[event['user_id']].append(event['action'])
         
-        # История действий по пользователям
-        user_behavior[event['user_id']].append(event['action'])
-        
-        # Данные сессий
-        sid = event['session_id']
-        # Если списка действий в этой сессии ещё нет — создаём его
-        if 'actions' not in session_data[sid]:
-            session_data[sid]['actions'] = []
-        session_data[sid]['actions'].append(event['action'])
-        # Если множества пользователей ещё нет — создаём
-        if 'users' not in session_data[sid]:
-            session_data[sid]['users'] = set()
-        session_data[sid]['users'].add(event['user_id'])
-        
-        # Активность по часам
-        event_hour = datetime.fromisoformat(event['timestamp']).hour # номер часа из ISO-строки
-        hourly_activity[event_hour] += 1
-
-        if event.get('price'):
-            revenue_data.append(event['price'])
-            
-        # Воронка конверсии
-        if action in ['view_product', 'add_to_cart', 'purchase', 'search']:
-            conversion_funnel[action] += 1
-            
-        # Эффективность продукта
-        product = event.get('product')
-        if product:
-            if action == 'view_product':
-                product_performance[product]['views'] += 1
-            elif action == 'add_to_cart':
-                product_performance[product]['cart_adds'] += 1
-            elif action == 'purchase':
-                product_performance[product]['purchases'] += 1
-                price = event.get('price')
-                if price:
-                    product_performance[product]['revenue'] += price
-
-        print(f"Message {message_count}: {event['action']} - {event.get('product', 'N/A')} - User {event['user_id']} - {event.get('price', 'N/A')}")
+        # Обработка сообщения (metrics.py)
+        metrics.process_event(event, message_count)
 
         # Показываем статистику каждые 10 сообщений
-        if message_count % 10 == 0:
-            print(f"\n--- Stats after {message_count} messages ---")
-            print("Top actions:")
-            for action, count in action_stats.most_common(3):
-                print(f"  {action}: {count}")
-            print(f"Active users: {len(user_sessions)}")
-            print("-" * 50)
+        if message_count % 20 == 0:
+            metrics.print_periodic_report(message_count)
 
 except KeyboardInterrupt:
-    print(f"\nStopped after {message_count} messages")
+    print(f"\nИтого:")
+    metrics.print_final_analytics(message_count)
+
+finally:
+    consumer.close()
     
-    # Анализ "История действий по пользователям"
-    if user_behavior:
-        # Словарь в список для DataFrame
-        user_behav_data = [(uid, act) for uid, acts in user_behavior.items() for act in acts]
-        df = pd.DataFrame(user_behav_data, columns=['user_id', 'action'])
-
-        # список - 5 частых посетителей
-        top_users = df.groupby('user_id').size().sort_values(ascending=False).head(5)
-        print(f'5 частых посетителей: \n{top_users}')
-
-    else:
-        print(f'\nИстория действий по пользователям не собрана!')
-        
-    # Анализ сессий
-    if session_data:
-        print(f'\nУникальных сессий: {len(session_data)}')
-    else:
-        print('\nДанные сессий не собраны!')
-        
-    # Анализ активности по часам
-    if hourly_activity:
-        for hour in sorted(hourly_activity.keys()):
-            print(f'{hour:02d}:00 - {hourly_activity[hour]} сообщений')
-    else:
-        print('\nНет данных активность по часам.')
-        
-    # Анализ Воронка конверсии
-    if conversion_funnel:
-        search = conversion_funnel['search']
-        views = conversion_funnel['view_product']
-        cart = conversion_funnel['add_to_cart']
-        purchases = conversion_funnel['purchase']
-
-        print(f'Поиск: {search}')
-        print(f'Просмотры: {views}')
-        print(f'Корзина: {cart}')
-        print(f'Покупки: {purchases}')
-
-        # Процент конверсии
-        if search > 0:
-            to_views_pct = (views / search) * 100
-            to_cart_pct = (cart / views) * 100
-            to_buy_pct = (purchases / cart) * 100 if cart > 0 else 0
-            print(f'\nКонверсия Поиск в Просмотр: {to_views_pct:.1f}%')
-            print(f'Конверсия Просмотр в Корзина: {to_cart_pct:.1f}%')
-            print(f'Конверсия Корзина в Покупка: {to_buy_pct:.1f}%')
-    else:
-        print('\nНет данных по воронке.')
-        
-    # Анализ Эффективность продукта
-    if product_performance:
-        df_products = pd.DataFrame(product_performance).T
-        # Сортируем по выручке для наглядности
-        df_products = df_products.sort_values(by='revenue', ascending=False)
-        print(f'Таблица Эффективность продукта: \n{df_products}')
-    else:
-        print(f'\nНет данных по Эффективность продукта')
---
+++ scripts/metrics.py
@@ -0,0 +1,157 @@
+from collections import defaultdict, Counter
+import pandas as pd
+from datetime import datetime
+
+class BusinessMetrics:
+    def __init__(self):
+        self.action_stats = Counter()
+        self.product_views = Counter()
+        self.user_sessions = defaultdict(list)
+
+        # Добавлены доп метрики для отслеживания
+        self.user_behavior = defaultdict(list) # История действий по пользователям
+        self.session_data = defaultdict(dict) # Данные сессий
+        self.hourly_activity = defaultdict(int) # Активность по часам
+        self.conversion_funnel = Counter() # Воронка конверсии
+        self.product_performance = defaultdict(lambda: {
+                'views': 0,
+                'cart_adds': 0,
+                'purchases': 0,
+                'revenue': 0
+            })
+
+        self.revenue_data = []
+        
+    def process_event(self, event, message_count):
+        action = event['action']
+        
+        # Обновляем статистику
+        self.action_stats[action] += 1
+
+        if event.get('product'):
+            self.product_views[event['product']] += 1
+        
+        user_id = event['user_id']
+        self.user_sessions[user_id].append(action)
+        # История действий по пользователям
+        self.user_behavior[user_id].append(action)
+        
+        # Данные сессий
+        sid = event['session_id']
+        # Если списка действий в этой сессии ещё нет — создаём его
+        if 'actions' not in self.session_data[sid]:
+            self.session_data[sid]['actions'] = []
+        self.session_data[sid]['actions'].append(action)
+        # Если множества пользователей ещё нет — создаём
+        if 'users' not in self.session_data[sid]:
+            self.session_data[sid]['users'] = set()
+        self.session_data[sid]['users'].add(user_id)
+        
+        # Активность по часам
+        event_hour = datetime.fromisoformat(event['timestamp']).hour # номер часа из ISO-строки
+        self.hourly_activity[event_hour] += 1
+
+        if event.get('price'):
+            self.revenue_data.append(event['price'])
+            
+        # Воронка конверсии
+        if action in ['view_product', 'add_to_cart', 'purchase', 'search']:
+            self.conversion_funnel[action] += 1
+            
+        # Эффективность продукта
+        product = event.get('product')
+        if product:
+            if action == 'view_product':
+                self.product_performance[product]['views'] += 1
+            elif action == 'add_to_cart':
+                self.product_performance[product]['cart_adds'] += 1
+            elif action == 'purchase':
+                self.product_performance[product]['purchases'] += 1
+                price = event.get('price')
+                if price:
+                    self.product_performance[product]['revenue'] += price
+        
+        print(f"Message {message_count}: {event['action']} - {event.get('product', 'N/A')} - User {event['user_id']} - {event.get('price', 'N/A')}")
+                    
+    def calculate_conversion_rates(self):
+        # Анализ Воронка конверсии
+        search = self.conversion_funnel['search']
+        views = self.conversion_funnel['view_product']
+        cart = self.conversion_funnel['add_to_cart']
+        purchases = self.conversion_funnel['purchase']
+
+        # Процент конверсии
+        to_views_pct = (views / search) * 100 if cart > 0 else 0
+        to_cart_pct = (cart / views) * 100 if cart > 0 else 0
+        to_buy_pct = (purchases / cart) * 100 if cart > 0 else 0
+        
+        return{
+            'search': search,
+            'views': views,
+            'cart_adds': cart,
+            'purchases': purchases,
+            'to_views_pct': round(to_views_pct, 2),
+            'to_cart_pct': round(to_cart_pct, 2),
+            'to_buy_pct': round(to_buy_pct, 2)
+        }
+        
+    def print_periodic_report(self, message_count):
+        rates = self.calculate_conversion_rates()
+        # Показываем статистику каждые 20 сообщений
+        
+        print(f"\n--- Stats after {message_count} messages ---")
+        print("Top actions:")
+        for action, count in self.action_stats.most_common(3):
+            print(f"  {action}: {count}")
+        print(f"Active users: {len(self.user_sessions)}")
+        print("-" * 50)
+        # Отображение Воронка конверсии
+        print('\nВоронка конверсии:')
+        print(f'Поиск: {rates['search']}')
+        print(f'Просмотры: {rates['views']}')
+        print(f'Корзина: {rates['cart_adds']}')
+        print(f'Покупки: {rates['purchases']}')
+        print(f'Конверсия Поиск в Просмотр: {rates['to_views_pct']}%')
+        print(f'Конверсия Просмотр в Корзина: {rates['to_cart_pct']}%')
+        print(f'Конверсия Корзина в Покупка: {rates['to_buy_pct']}%')
+        print("-" * 50)
+        
+    def print_final_analytics(self, message_count):
+        print(f"\nStopped after {message_count} messages")
+        print("-" * 50)
+        
+        # Анализ "История действий по пользователям"
+        if self.user_behavior:
+            # Словарь в список для DataFrame
+            user_behav_data = [(uid, act) for uid, acts in self.user_behavior.items() for act in acts]
+            df = pd.DataFrame(user_behav_data, columns=['user_id', 'action'])
+            # список - 5 частых посетителей
+            top_users = df.groupby('user_id').size().sort_values(ascending=False).head(5)
+            print(f'5 частых посетителей: \n{top_users}')
+            
+        # Анализ сессий
+        print(f'\nУникальных сессий: {len(self.session_data)}')
+        
+        # Анализ активности по часам
+        for hour in sorted(self.hourly_activity.keys()):
+            print(f'{hour:02d}:00 - {self.hourly_activity[hour]} сообщений')
+        
+        # Отображение Воронка конверсии
+        rates = self.calculate_conversion_rates()
+        print('\nВоронка конверсии:')
+        print(f'Поиск: {rates['search']} шт.')
+        print(f'Просмотры: {rates['views']} шт.')
+        print(f'Корзина: {rates['cart_adds']} шт.')
+        print(f'Покупки: {rates['purchases']} шт.')
+        print(f'Конверсия Поиск в Просмотр: {rates['to_views_pct']}%')
+        print(f'Конверсия Просмотр в Корзина: {rates['to_cart_pct']}%')
+        print(f'Конверсия Корзина в Покупка: {rates['to_buy_pct']}%')
+        
+        # Анализ Эффективность продукта
+        if self.product_performance:
+            df_products = pd.DataFrame(self.product_performance).T
+            # Сортируем по выручке для наглядности
+            df_products = df_products.sort_values(by='revenue', ascending=False)
+            print(f'\nТаблица Эффективность продукта: \n{df_products}')
+
+            
